d9c72c840566ff908ab8e8e40b36088c918cc940
Stavros Tsogkas <statsogk@gmail.com>
2016-11-21 17:41:59 -0500

    vectorized for loops in set cover algorithm

--- amatSetCoverGreedy.m
+++ amatSetCoverGreedy.m
@@ -5,7 +5,7 @@ errorType = 'se';
 encodingType = 'average';
 colorWeights = [];
 
-% img0 = im2double(imresize(imread('google.jpg'), [128 128], 'nearest')); 
+% imgRGB = im2double(imresize(imread('google.jpg'), [128 128], 'nearest')); 
 imgRGB = im2double(imresize(imread('/home/tsogkas/datasets/BSDS500/images/train/66075.jpg'), [128 128], 'nearest')); 
 [H,W,numChannels] = size(imgRGB);
 imgLab = rgb2labNormalized(imgRGB);
@@ -27,7 +27,7 @@ reconstructionError = imageError(img,f,filters,errorType,colorWeights);
 
 %% Greedy approximation of the weighted set cover problem associated with AMAT
 % Initializations
-amat.input          = imgRGB;
+amat.input          = reshape(imgRGB, H*W, numChannels);
 amat.reconstruction = reshape(amat.input, H*W, numChannels);
 amat.axis           = zeros(H,W,numChannels);
 amat.radius         = zeros(H,W);
@@ -35,7 +35,20 @@ amat.depth          = zeros(H,W); % #disks points(x,y) is covered by
 amat.covered        = false(H,W);
 amat.price          = inf(H,W);   % error contributed by each point
 
-% Easy way to compute the number of NEW pixels that will be covered by each 
+
+%% Create sparse adjacency matrix for disk inclusions.
+% A is a H*W x H*W x R array, with A(i,j,r) == 1 iff the disk that is
+% centered at pixel i and has radius r, covers pixel j.
+[x,y] = meshgrid(1:W,1:H); 
+xx2 = bsxfun(@minus,x(:),x(:)').^2;
+yy2 = bsxfun(@minus,y(:),y(:)').^2;
+coveredByDisk = false(H*W,H*W,numScales);
+for r=1:numScales
+    coveredByDisk(:,:,r) = xx2 + yy2 <= r^2;
+end
+clear xx2 yy2
+
+% Easy way to compute the number of NEW pixels that will be covered by each
 % disk if it is added in the solution, taking into account the fact that
 % larger disks exceed image boundaries.
 numNewPixelsCovered = ones(H,W,numScales);
@@ -43,6 +56,7 @@ for r=1:numScales
     numNewPixelsCovered(:,:,r) = conv2(numNewPixelsCovered(:,:,r), double(filters{r}),'same');
 end
 numNewPixelsCovered = reshape(numNewPixelsCovered ,H*W,numScales);
+
 
 %% Error balancing and visualization of top (low-cost) disks
 % -------------------------------------------------------------------------
@@ -64,19 +78,18 @@ numNewPixelsCovered = reshape(numNewPixelsCovered ,H*W,numScales);
 % -------------------------------------------------------------------------
 % The final recursive formula for the regularization term is:
 % REG(r_R) = 0; REG(r_i) = T*e_i + (N_i / N_{i+1})*REG_{i+1}, for i=1,...,R-1
-T = 0.12;
+T = 0.0001;
 diskCost = reshape(reconstructionError, H*W,numScales);
-REG = diskCost ./ numNewPixelsCovered; REG(:,end) = 0; 
-REG = T * numNewPixelsCovered .* cumsum(REG ,2,'reverse');
-diskCost = diskCost + REG;
-diskCostEffective = diskCost ./ numNewPixelsCovered;
 freshaped = reshape(f, [],numChannels,numScales);
+REG = [squeeze(mean(diff(freshaped,1,3).^2 ,2)), zeros(H*W,1)]; 
+REG = bsxfun(@plus, REG, (numScales:-1:1)*T);
+diskCostEffective = diskCost ./ numNewPixelsCovered + REG;
 % Sort costs in ascending order and visualize top disks.
 [sortedCosts, indSorted] = sort(diskCostEffective(:),'ascend');
 top = 1e2;
 [yy,xx,rr] = ind2sub([H,W,numScales], indSorted(1:top));
 figure(1); imshow(amat.input); 
-viscircles([xx,yy],rr,'Color','w','LineWidth',0.5);
+viscircles([xx,yy],rr,'Color','k','LineWidth',0.5);
 viscircles([xx(1),yy(1)],rr(1),'Color','b','EnhanceVisibility',false); 
 title(sprintf('W: Top-%d disks, B: Top-1 disk',top))
 
@@ -102,6 +115,8 @@ while ~all(amat.covered(:))
     amat.axis(yc,xc,:) = f(yc,xc,:,rc);
     amat.radius(yc,xc) = rc;
 
+    
+    
     % TL;DR: Subtract newly covered pixels from all overlapping disks. 
     % LONG STORY: We have decided to place a disk centered at (xc,yc)
     % with radius r. This disks covers all points (xd,yd) that lie within
@@ -111,7 +126,20 @@ while ~all(amat.covered(:))
     % Each (xd,yd) is covered by all disks that have centers at distance <=
     % maxRadius from them.
     % find coordinates of all newly covered pixels inside D
-    [yd,xd] = ind2sub([H,W],find(newPixelsCovered)); 
+    [yd,xd] = ind2sub([H,W],find(newPixelsCovered)); % #pixels x 1
+    xx2 = bsxfun(@minus,xd,x(:)').^2; % #pixels x H*W
+    yy2 = bsxfun(@minus,yd,y(:)').^2; % #pixels x H*W
+    % isCoveredByDisk: #pixels x H*W x numScales
+    % isCoveredByDisk(i,j,r): pixel i is covered by a disk centered at
+    % pixel j, with radius r. 
+    % sum(isCoveredByDisk,1): #pixels covered by pixel j at radius r
+    isCoveredByDisk = bsxfun(@le,xx2+yy2,reshape((1:numScales).^2,1,1,[])); 
+    numNewPixelsCovered = numNewPixelsCovered - squeeze(sum(isCoveredByDisk));
+    % Remove error caused by pixels that are covered by the selected disk.
+    
+    
+    
+    
     for i=1:numel(yd)
         distFromCoveredPointSquared = (x-xd(i)).^2 + (y-yd(i)).^2;
         % All the points that lie within max radius distance from the 
@@ -126,7 +154,7 @@ while ~all(amat.covered(:))
             numNewPixelsCovered(inds, r) = numNewPixelsCovered(inds, r) - 1;
             diskCost(inds,r) = diskCost(inds,r) - ...
                 mean(bsxfun(@minus, freshaped(inds,:,r), ... % subtract old cost for pixel
-                reshape(amat.input(yd(i),xd(i),:), [1 3])).^2, 2);
+                amat.input(sub2ind([H,W],yd(i),xd(i)),:)).^2, 2);
 %             freshaped(inds,:,r) = freshaped(inds,:,r) - 
         end        
     end
@@ -141,7 +169,7 @@ while ~all(amat.covered(:))
         % Sort costs in ascending order to visualize updated top disks.
         [sortedCosts, indSorted] = sort(diskCostEffective(:),'ascend');
         [yy,xx,rr] = ind2sub([H,W,numScales], indSorted(1:top));
-        subplot(221); imshow(amat.input); 
+        subplot(221); imshow(reshape(amat.input, H,W)); 
         viscircles([xc,yc],rc, 'Color','k','EnhanceVisibility',false);
         title('Selected disk');
         subplot(222); imshow(bsxfun(@times, amat.input, double(~amat.covered))); 
