5b8b674b17eaaebec2980be8d12e68ff0d1e14f8
Stavros <statsogk@gmail.com>
2017-02-07 03:03:37 -0500

    added constructBMAX500() to create medial axes groundtruth and store it as a mat file

--- setPaths.m
+++ setPaths.m
@@ -6,9 +6,13 @@ paths.root = fileparts(mfilename('fullpath'));
 paths.data             = fullfile(paths.root, 'data');
 paths.output           = fullfile(paths.root, 'output');
 paths.bsds500          = fullfile(paths.data,'BSDS500');
-paths.bsds500gtTrain   = fullfile(paths.bsds500,'groundtruth','train');
-paths.bsds500gtTest    = fullfile(paths.bsds500,'groundtruth','test');
-paths.bsds500gtVal     = fullfile(paths.bsds500,'groundtruth','val');
+paths.bsds500gt        = fullfile(paths.bsds500, 'groundtruth');
+paths.bsds500im        = fullfile(paths.bsds500, 'images');
+
+% TODO: maybe remove the following to reduce clutter?
+paths.bsds500gtTrain   = fullfile(paths.bsds500gt,'train');
+paths.bsds500gtTest    = fullfile(paths.bsds500gt,'test');
+paths.bsds500gtVal     = fullfile(paths.bsds500gt,'val');
 paths.bsds500imTrain   = fullfile(paths.bsds500,'images','train');
 paths.bsds500imTest    = fullfile(paths.bsds500,'images','test');
 paths.bsds500imVal     = fullfile(paths.bsds500,'images','val');
