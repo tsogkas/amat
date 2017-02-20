0ecef1a01aba7fc87a241653b5cb823a4a7c7900
Stavros Tsogkas <statsogk@gmail.com>
2017-02-19 22:36:29 -0500

    fixed bugs and optimized amat(), writing code for testing imageReconstruction

--- testReconstruction.m
+++ testReconstruction.m
@@ -1,4 +1,4 @@
-function testReconstruction()
+function testReconstruction(models,varargin)
 
 % Default testing options ------------------------------------------------
 opts = {'dataset',   'BSDS500',...
@@ -11,11 +11,9 @@ opts = parseVarargin(opts,varargin,'struct');
 paths = setPaths();
 if ischar(opts.set) && strcmp(opts.set, 'val')
     imPath    = fullfile(paths.bsds500im,'val');
-    gtPath    = fullfile(paths.symmax500,'val');
     imageList = dir(fullfile(imPath, '*jpg'));
 elseif ischar(opts.set) && strcmp(opts.set, 'test')
     imPath    = fullfile(paths.bsds500im,'test');
-    gtPath    = fullfile(paths.symmax500,'test');
     imageList = dir(fullfile(imPath, '*jpg'));
 elseif isstruct(opts.set)
     disp('Data provided in struct form')
@@ -32,12 +30,100 @@ opts.nImages = numel(imageList);
 if ~iscell(models), models = {models}; end
 for m=1:numel(models)
     switch lower(models{m})
-        case 'levinstein'
-            models{m} = loadLevinsteinModel(models{m},paths);
-        case 'lindeberg'
+        case 'amat'
             models{m} = struct('name',models{m});
         otherwise % load MIL or CNN model
             models{m} = loadModelFromMatFile(models{m},paths);
     end
+    models.stats.mse = zeros(opts.nImages, 4);
+    models.stats.psnr= zeros(opts.nImages, 4);
+    models.stats.ssim= zeros(opts.nImages, 4);
 end
 
+% Evaluate models on test images and compute approximate reconstructions --
+ticStart = tic;
+for i=1:opts.nImages
+    if isfield(imageList(i), 'isdir')
+        % Load image and groundtruth data from disk
+        [~,iid,~] = fileparts(imageList(i).name);
+        img = im2double(imread(fullfile(imPath,imageList(i).name)));
+    else % Read image and groundtruth from struct
+        img = imageList(i).img;
+        iid = imageList(i).iid;
+    end
+    
+    clear features 
+    for m=1:numel(models)
+        switch models{m}.name
+            case 'amat'
+                rec = reconstructionAMAT(img);
+            case 'deepskel'
+                rec = reconstructionDeepSkel(models{m},img)
+            otherwise % MIL 
+                rec = reconstructionMIL(models{m},img);
+        end
+        [models{m}.stats.cntP(i,:), models{m}.stats.sumP(i,:),...
+         models{m}.stats.cntR(i,:), models{m}.stats.sumR(i,:),...
+         models{m}.stats.scores(i,:)] = computeImageStats(spb,gt,opts);
+    end
+    msg = sprintf('Testing on BSDS500 %s set\n', opts.set);
+    progress(msg,i,opts.nImages,ticStart,1);
+end
+
+% Compute dataset-wide stats
+for m=1:numel(models)
+    [models{m}.stats.oidP,  models{m}.stats.oidR, ...
+     models{m}.stats.oidF,  models{m}.stats.oidT, ...
+     models{m}.stats.oisP,  models{m}.stats.oisR, ...
+     models{m}.stats.oisF,  models{m}.stats.AP] = ...
+        computeDatasetStats(models{m}.stats, opts);
+    % Create field with dataset-specific stats
+    models{m}.(opts.dataset).(opts.set).stats = stats;
+    models{m}.(opts.dataset).(opts.set).opts = opts;
+    models{m} = rmfield(models{m},'stats');
+    % And store results
+    modelPath = fullfile(paths.sbpmil.models, models{m}.name);
+    model = models{m}; save(modelPath, 'model')
+end
+
+% -------------------------------------------------------------------------
+function rec = reconstructionAMAT(img)
+% -------------------------------------------------------------------------
+mat = amat(img);
+rec = mat.reconstruction;
+
+% -------------------------------------------------------------------------
+function rec = reconstructionDeepSkel(model,img)
+% -------------------------------------------------------------------------
+rfields = [0,14,40,92,196];
+net = model.net;
+img = bsxfun(@minus, single(img), reshape(net.meta.averageImage,1,1,[]));
+net.eval({'input',img});
+lout= net.getLayer(net.getLayerIndex('softmax'));
+spb = net.vars(lout.outputIndexes); % symmetry probabilities
+spb = spb.value > threshold; % set this to the optimal threshold (contained in model struct)
+[~,scales] = max(spb.value,[],3); % estimate scales
+scales = rfields(scales);
+
+% -------------------------------------------------------------------------
+function rec = reconstructionMIL(model,img)
+% -------------------------------------------------------------------------
+spb = spbMIL(img, 'featureSet',model.opts.featureSet,'w',model.w);
+spb = spb.thin;
+
+
+
+% -------------------------------------------------------------------------
+function model = loadModelFromMatFile(model,paths)
+% -------------------------------------------------------------------------
+if exist(fullfile(paths.spbmil.models, model),'file') % MIL model
+    tmp = load(fullfile(paths.spbmil.models, model));
+elseif exist(model,'file')
+    tmp = load(model);
+end
+if isfield(tmp,'model')     % MIL model
+    model = tmp.model;
+elseif isfield(tmp,'net')   % DagNN model
+    model = struct('trainStats',tmp.stats, 'name','deepskel',...
+                   'net',dagnn.DagNN.loadobj(tmp.net));
+end
