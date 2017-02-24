333e9e240f793cbfc2d3e81d1d346bdc3f85ba77
Stavros Tsogkas <statsogk@gmail.com>
2017-02-24 17:21:44 -0500

    updated grouping code

--- groupMedialPoints.m
+++ groupMedialPoints.m
@@ -14,13 +14,16 @@ end
 % Grouping scheme
 % - TODO: Maybe use a criterion that finds the most appropriate group to
 %   merge with, in case of multiple matches.
-branches = zeros(H,W,'uint8');
+% - TODO: when I merge two groups, I have to also merge children groups
 mask = false(H,W);
 maxLabel = 1;
-for r=1:40
+
+% Scales loop -------------------------------------------------------------
+for r=1:R 
 %     figure(1); imshow2(sum(rad(:,:,1:r-1),3), rad(:,:,r));
     cc{r}.labels = zeros(1, cc{r}.NumObjects); % zero for non-examined ccs
-    margin = ceil(r);
+    margin = ceil(0.5*r);
+    % CC loop -------------------------------------------------------------
     for i=1:cc{r}.NumObjects;
         % Create rectangle area around cc for efficiency
         [y,x] = ind2sub([H,W], cc{r}.PixelIdxList{i});
@@ -30,48 +33,51 @@ for r=1:40
         mask(:) = false;
         mask(cc{r}.PixelIdxList{i}) = true;
         mask(ymin:ymax,xmin:xmax) = bwdist(mask(ymin:ymax,xmin:xmax)) <= r+margin;
+        
         % The cc is assigned a new label, unless it has already been merged 
         if cc{r}.labels(i) == 0
             cc{r}.labels(i) = maxLabel;
-            for rr=(r-1):-1:max(1,r-5)  % First merge ccs of smaller radii 
-                for j=1:cc{rr}.NumObjects
-                    % merge ccs that are close enough. If cc is matched 
-                    % with multiple components, these should have the 
-                    % same label anyway.
-                    if cc{r}.labels(i) == maxLabel && ...
-                       any(mask((cc{rr}.PixelIdxList{j})))
-%                         figure(2); plotMergedBranches(cc{r}.PixelIdxList{i},cc{rr}.PixelIdxList{j});
-                        cc{r}.labels(i) = cc{rr}.labels(j);
-                    end
-                end
-            end
-            % If the component has not been merged, increase maxLabel
-            if cc{r}.labels(i) == maxLabel
-                maxLabel = maxLabel + 1; 
-            end
         end
         
-        % Now merge ccs at the same scale 
-        for j=i+1:cc{r}.NumObjects
-            if cc{r}.labels(i) ~= cc{r}.labels(j) && any(mask(cc{r}.PixelIdxList{j}))
-%                 figure(2); plotMergedBranches(cc{r}.PixelIdxList{i},cc{r}.PixelIdxList{j});
+        % Find groups at smaller scales that can potentially be merged 
+        mergedLabels = cc{r}.labels(i);
+        for rr=(r-1):-1:max(1,r-3)
+            for j=1:cc{rr}.NumObjects
+                if ~any(mergedLabels == cc{rr}.labels(j)) && any(mask(cc{rr}.PixelIdxList{j}))
+                    mergedLabels = [mergedLabels, cc{rr}.labels(j)];
+                end
+            end
+        end    
+        
+        % Merge labels (use the smallest label as the common label)
+        commonLabel = min(mergedLabels);
+        for rr=1:r
+            cc{rr}.labels(ismember(cc{rr}.labels, mergedLabels)) = commonLabel;
+        end
+                        
+        % Merge ccs at the same scale 
+        for j=(i+1):cc{r}.NumObjects
+            if any(mask(cc{r}.PixelIdxList{j}))
                 cc{r}.labels(j) = cc{r}.labels(i);
             end
         end
+        
+        % If the component has not been merged, increase maxLabel
+        if cc{r}.labels(i) == maxLabel
+            maxLabel = maxLabel + 1;
+        end
+        
     end
-    % Update label map
+end
+
+% Construct label map
+branches = zeros(H,W,'uint8');
+for r=1:R
     for i=1:cc{r}.NumObjects
         branches(cc{r}.PixelIdxList{i}) = cc{r}.labels(i);
     end
 end
 
-% Nested utility functions ------------------------------------------------
-%     function out = isClose(ccidx)
-%         [yy,xx] = ind2sub([H,W], ccidx);
-%         xcontained = xx >= xmin & xx <= xmax;
-%         ycontained = yy >= ymin & yy <= ymax;
-%         out = any(xcontained & ycontained);
-%     end
 
     function plotMergedBranches(cc1,cc2)
         tmp = zeros(H,W,'uint8');
