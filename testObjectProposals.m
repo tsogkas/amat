a7e07b72b482d43c3ed13dd9bb9dd9395a65e0da
Stavros <statsogk@gmail.com>
2017-03-06 03:05:02 -0500

    added modeified edgeBoxes() function

--- testObjectProposals.m
+++ testObjectProposals.m
@@ -1,7 +1,7 @@
-function models = testObjectProposals(models,varargin)
+function method = testObjectProposals(method,varargin)
 
 % Default testing options ------------------------------------------------
-if nargin < 1, models = 'amat'; end
+if nargin < 1, method = 'edges'; end
 opts = {'dataset',      'VOC2007',...
         'set',          'val',...   % 'val' or 'test'
         'visualize',    false,...
@@ -13,25 +13,40 @@ opts = parseVarargin(opts,varargin,'struct');
 % Setup paths
 paths  = setPaths;
 imgDir = fullfile(paths.voc2007, 'VOC2007/JPEGImages/');
-imgList=fullfile(paths.voc2007, ['VOC2007/ImageSets/Main/' set '.txt']);
+imgList=fullfile(paths.voc2007, ['VOC2007/ImageSets/Main/' opts.set '.txt']);
 addpath(genpath(fullfile(paths.voc2007, 'VOCcode')));
+mkdir(paths.amat.boxes);
 
 % get list of image ids
 if(~exist(imgList,'file')), error('ids file not found'); end
 f=fopen(imgList); ids=textscan(f,'%s %*s'); ids=ids{1}; fclose(f);
 
-data=boxesData('resDir',paths.amat.boxes,'dataDir',paths.voc2007','split',set);
-nm='EdgeBoxes70'; opts.name=fullfile(paths.amat.boxes, [nm '-' set '.mat']);
-edgeBoxes(data.imgs,model,opts); opts.name=[];
+% Load VOC2007 groundtruth boxes
+data=boxesData('resDir',[paths.amat.boxes filesep],...
+    'dataDir',[paths.voc2007 filesep],'split',opts.set);
+
+% Load structured edges model
+model=load(fullfile(paths.edges.root, 'models/forest/modelBsds')); 
+model=model.model;
+model.opts.multiscale=0; model.opts.sharpen=2; model.opts.nThreads=4;
+
+% Load default edge boxes parameters
+ebopts = edgeBoxes;
+ebopts.alpha = .65;     % step size of sliding window search
+ebopts.beta  = .75;     % nms threshold for object proposals
+ebopts.minScore = .01;  % min score of boxes to detect
+ebopts.maxBoxes = 1e4;  % max number of boxes to detect
+ebopts.method = method; 
+switch method
+    case 'amat-edges'
+        nm='AmatEdgeBoxes';
+    case 'AmatAxesBoxes';
+        nm='AmatAxes';
+    case 'edges'
+        nm='EdgeBoxes70'; 
+    otherwise, error('Method not supported.')
+end
+ebopts.name=fullfile(paths.amat.boxes, [nm '-' opts.set '.mat']);
+edgeBoxes(data.imgs,model,ebopts); ebopts.name=[];
 boxesEval('data',data,'names',nm,'thrs',.7,'show',2);
 boxesEval('data',data,'names',nm,'thrs',.5:.05:1,'cnts',1000,'show',3);
-
-% -------------------------------------------------------------------------
-function model = loadModelFromMatFile(model,paths)
-% -------------------------------------------------------------------------
-if exist(fullfile(paths.amat.models, model),'file') || ...
-   exist([fullfile(paths.amat.models, model) '.mat'],'file')
-    tmp = load(fullfile(paths.amat.models, model)); model = tmp.model;
-else
-    model = struct('name',model); 
-end
